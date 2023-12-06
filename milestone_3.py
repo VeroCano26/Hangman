005fbfac19c3799174fdbe6c0515e16a4639b026
Veronica Cano Rodriguez <vero@MacBook-Pro-de-Veronica.local>
2023-12-06 18:37:21 +0000

    Add code Milestone 5

---
+++ milestone_3.py
@@ -0,0 +1,41 @@
+# Hangman Project: Milestone 3 - Task 1
+
+while True:
+    guess = input("Guess a letter: ")
+
+    if guess.isalpha() and len(guess) == 1:
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+
+# Hangman Project: Milestone 3 - Task 2
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+# Hangman Project: Milestone 3 - Task 3
+
+def check_guess(guess):
+    guess = guess.lower()
+    
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Enter a single letter: ")
+        check_guess(guess)
+        break
+
+ask_for_input()
+
+# Hangman Project: Milestone 2 - Task 4 - ReadMe file
+
+# Hangman Project: Milestone 2 - Task 5 - Update code to GitHub
+
+# Hangman Project: Milestone 2 - Task 6 - Optimise current code 
--- milestone_5.py
+++ milestone_5.py
@@ -11,12 +11,12 @@ class Hangman:
 
     def pick_random_word(self):
         return random.choice(self.word_list)
-    
-def check_guess(self, guess):
+
+    def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-
+            
             for i, letter in enumerate(self.word):
                 if letter == guess:
                     self.word_guessed[i] = guess
@@ -26,7 +26,7 @@ def check_guess(self, guess):
             print(f"Sorry, {guess} is not in the word.")
             print(f"You have {self.num_lives} lives left.")
 
-def ask_for_input(self):
+    def ask_for_input(self):
         while True:
             guess = input("Guess a letter: ")
             if not guess.isalpha() or len(guess) != 1:
@@ -37,3 +37,17 @@ def ask_for_input(self):
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
                 break
+
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+    
+    while True:
+        if game.num_lives == 0:
+            print('You lost!')
+            break
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print('Congratulations. You won the game!')
+            break
